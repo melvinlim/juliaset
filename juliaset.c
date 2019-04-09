5bf2239f44e22c682ee4eabb337ce0d2d898eac7
Melvin Lim <melvinlim@MacBook.local>
2019-04-09 10:03:15 -0700

    fixed equations...  and added color

--- juliaset.c
+++ juliaset.c
@@ -12,12 +12,13 @@
 #define CXINIT -0.835
 #define CYINIT -0.230
 
-#define MAXITER 255
+#define MAXITER 192
+//#define HISTOGRAM
 
 SDL_Surface *screen;
 
 void draw(const double cx,const double cy){
-	int histogram[256]={0};
+	int histogram[MAXITER]={0};
 	int iter;
 	double zx,zy,zxs,zys;
 	double tmp;
@@ -35,12 +36,10 @@ void draw(const double cx,const double cy){
 			zys=zy*zy;
 			while(((zxs+zys)<4.0)&&(iter<MAXITER)){
 				tmp=zxs-zys;
-//				zy=2*zx*zy+cy;
-//				zx=tmp+cx;
-				zys=2*zx*zy+cy;
-				zys=zys*zys;
-				zxs=tmp+cx;
-				zxs=zxs*zxs;
+				zy=2*zx*zy+cy;
+				zx=tmp+cx;
+				zys=zy*zy;
+				zxs=zx*zx;
 				iter++;
 			}
 			histogram[iter]++;
@@ -62,20 +61,33 @@ void draw(const double cx,const double cy){
 //					printf("%d %d: %f\n",i,j,red);
 			}
 */
-			if(iter<=90)
-				red=green=blue=iter/90.0*255;
-			else
-				red=green=blue=0;
+			if(iter<=64){
+				red=iter*4-1;
+				green=blue=0;
+			//	red=green=blue=iter/90.0*255;
+			}else if(iter<=128){
+				green=(iter-64)*4-1;
+				red=255;
+				red=0;
+				blue=0;
+			}else{
+				blue=(iter-128)*4-1;
+				red=green=255;
+				red=green=0;
+				//red=green=blue=0;
+			}
 			*((Uint32*)screen->pixels+i*XLEN+j)=SDL_MapRGBA(screen->format,red,green,blue,ALPHA);
 //			*((Uint32*)screen->pixels+i*XLEN+j)=SDL_MapRGBA(screen->format,i*255.0/YLEN,j*255.0/XLEN,255-i*255.0/YLEN,ALPHA);
 		}
 	}
 
-//	for(int i=0;i<MAXITER;i++){
-//		if(histogram[i]>0){
-//			printf("%d:%d\n",i,histogram[i]);
-//		}
-//	}
+#ifdef HISTOGRAM
+	for(int i=0;i<MAXITER;i++){
+		if(histogram[i]>0){
+			printf("%d:%d\n",i,histogram[i]);
+		}
+	}
+#endif
 
 	if(SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen);
 	//  SDL_Flip(screen); 
